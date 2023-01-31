49c45b525d648edd714db0316916268612b654ab
anuLoveCoading <anusrivas07@gmail.com>
2023-01-31 20:52:12 +0530

    Tree_node project

--- src/tee_1.java
+++ src/tee_1.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class tee_1 {
+    int val;
+    tee_1 left;
+    tee_1 right;
+
+    tee_1(int val, tee_1 left, tee_1 right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello tree");
+        Scanner scn = new Scanner(System.in);
+
+        tee_1 tree_node = new tee_1(1,null,null);
     }
 }
