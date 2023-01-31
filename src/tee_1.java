6b45c79144eb05b28a3c65eaa1fe5e3bca9bc21e
anuLoveCoading <anusrivas07@gmail.com>
2023-01-31 21:01:10 +0530

    Tree_node project

--- src/tee_1.java
+++ src/tee_1.java
@@ -10,10 +10,29 @@ public class tee_1 {
         this.left = left;
         this.right = right;
     }
+    public static void preOrder(tee_1 tree_node){
 
+    }
+
+    public static void inOrder(tee_1 tree_node){
+
+    }
+
+    public static void poOrder(tee_1 tree_node){
+
+    }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
         tee_1 tree_node = new tee_1(1,null,null);
+        tee_1 tree_node_left = new tee_1(6,null,null);
+        tee_1 tree_node_right = new tee_1(2,null,null);
+        tee_1 tree_node_right_left = new tee_1(3,null,null);
+
+        tree_node.left = tree_node_left;
+        tree_node.right = tree_node_right;
+
+        tree_node_right.left = tree_node_right_left;
+
     }
 }
