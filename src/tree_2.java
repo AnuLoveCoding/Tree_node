5a0bc4257e34aad0a97d29a55594d4ab86bd2cc2
anuLoveCoading <anusrivas07@gmail.com>
2023-02-09 13:35:58 +0530

    Tree_node project

--- src/tree_2.java
+++ src/tree_2.java
@@ -9,12 +9,56 @@ public class tree_2 {
         this.right = right;
     }
 
+    public static void preOrder(tree_2 tree){
+        if(tree == null){
+            return;
+        }
+        System.out.println("tree.data = "+tree.data);
+        preOrder(tree.left);
+        preOrder(tree.right);
+    }
+
+    public static void inOrder(tree_2 tree){
+        if(tree == null){
+            return;
+        }
+
+        inOrder(tree.left);
+        System.out.println("tree.data = " + tree.data);
+        inOrder(tree.right);
+    }
+
+    public static void poOrder(tree_2 tree){
+        if(tree == null){
+            return;
+        }
+
+        poOrder(tree.left);
+        poOrder(tree.right);
+        System.out.println("tree.data = "+ tree.data);
+    }
+
     public static void main(String[] args) {
         tree_2 tree = new tree_2(10,null,null);
         tree_2 tree_left = new tree_2(15,null,null);
         tree_2 tree_right = new tree_2(18,null,null);
+        tree_2 tree_left_left = new tree_2(22,null,null);
+        tree_2 tree_left_right = new tree_2(64,null,null);
+        tree_2 tree_right_left = new tree_2(41,null,null);
+        tree_2 tree_right_right = new tree_2(56,null,null);
 
         tree.left = tree_left;
-        tree_right = tree_right;
+        tree.right = tree_right;
+
+        tree_left.left = tree_left_left;
+        tree_left.right = tree_left_right;
+
+        tree_right.left = tree_right_left;
+        tree_right.right = tree_right_right;
+
+        preOrder(tree);
+        inOrder(tree);
+        poOrder(tree);
+
     }
 }
