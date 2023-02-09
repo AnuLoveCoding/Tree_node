8e2d9ef5a23e73fe5c18661c63da905f3b0fa75e
anuLoveCoading <anusrivas07@gmail.com>
2023-02-09 18:45:26 +0530

    git commit -m "Tree_project"

--- src/tree_3.java
+++ src/tree_3.java
@@ -9,6 +9,33 @@ public class tree_3 {
         this.right = right;
     }
 
+    public static void Inorder(tree_3 tree){
+        if(tree == null){
+            return;
+        }
+        Inorder(tree.left);
+        System.out.println(tree.data);
+        Inorder(tree.right);
+    }
+
+    public static void PoOrder(tree_3 tree){
+        if(tree == null){
+            return;
+        }
+        PoOrder(tree.left);
+        PoOrder(tree.right);
+        System.out.println(tree.data);
+    }
+
+    public static void preorder(tree_3 tree){
+        if(tree == null){
+            return;
+        }
+        System.out.println(tree.data);
+        preorder(tree.left);
+        preorder(tree.right);
+    }
+
     public static void main(String[] args) {
         tree_3 tree = new tree_3(55,null,null);
         tree_3 tree_left = new tree_3(10,null,null);
@@ -31,7 +58,9 @@ public class tree_3 {
 
         tree_right_right.left = tree_right_right_left;
 
-        
+        Inorder(tree);
+//        PoOrder(tree);
+//        preorder(tree);
 
     }
 }
