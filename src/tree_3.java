b690996df5fc5b063bd1e29a9411c3d48938ebd2
anuLoveCoading <anusrivas07@gmail.com>
2023-02-09 18:37:34 +0530

    git commit -m "Tree_project"

--- src/tree_3.java
+++ src/tree_3.java
@@ -8,13 +8,28 @@ public class tree_3 {
         this.left = left;
         this.right = right;
     }
+
     public static void main(String[] args) {
         tree_3 tree = new tree_3(55,null,null);
         tree_3 tree_left = new tree_3(10,null,null);
         tree_3 tree_right = new tree_3(61,null,null);
+        tree_3 tree_left_left = new tree_3(24,null,null);
+        tree_3 tree_left_right = new tree_3(68,null,null);
+        tree_3 tree_right_right = new tree_3(25,null,null);
+        tree_3 tree_left_right_right = new tree_3(3,null,null);
+        tree_3 tree_right_right_left = new tree_3(40,null,null);
 
         tree.left = tree_left;
         tree.right = tree_right;
+
+        tree_left.left = tree_left_left;
+        tree_left.right = tree_left_right;
+
+        tree_left_right.right = tree_left_right_right;
+
+        tree_right.right = tree_right_right;
+
+        tree_right_right.left = tree_right_right_left;
 
         
 
